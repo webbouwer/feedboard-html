d525aef3c345e2024002e3e95267ad6d1c49b896
Oddsized <oddsized@gmail.com>
2016-12-21 09:07:53 +0100

    Create feeds.js

---
+++ feeds.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Feedboard 0.5 
+ * 
+ */
+
+var FeedBoard = function() {};
+FeedBoard.prototype = {
+	
+	checkClient: function(){
+	  // check device variables
+	  // (control frameworks here)
+  }
+  
+};
+
+
+
+
+
+window.onload = function() {
+
+	var usrBoard = new FeedBoard();
+	var usrClient = usrApp.checkClient();
+  
+};
